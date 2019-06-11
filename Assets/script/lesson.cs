ea76c7143e378b22f69abcc13c9a1a6941a3edde
u811286 <21811286tf@tama.ac.jp>
2019-06-11 11:39:32 +0900

    First Commit
    Commit test

---
+++ Assets/script/lesson.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lesson : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = 3;
+        if (a < 5)
+        {
+            print("aは5より小さい");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
